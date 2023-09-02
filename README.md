e5e78dd45c7e88ac4e2e343bf260d6aab5bcb27f
Danylo Vashchilenko <dan.vashchilenko@gmail.com>
2023-09-02 21:18:33 +0000

    update vim plugins and tmux settings

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Humus is an Ubuntu-based container image with an opinionated set of software pre-installed.
 
-## Components:
+## Core Components
 * Ubuntu LTS
 * ZSH with oh-my-zsh
 * Vim with plugins
@@ -10,10 +10,17 @@ Humus is an Ubuntu-based container image with an opinionated set of software pre
 
 ## Local Installation
 ```
-HUMUS_PATH="~/opt/humus"
+HUMUS_PATH="$HOME/opt/humus"
 
 cp -r source $HUMUS_PATH
 $HUMUS_PATH/vim/install.sh
 $HUMUS_PATH/zsh/install.sh
 $HUMUS_PATH/symlink.sh $HUMUS_PATH
 ```
+
+## Additional Tools
+
+* `bmon` for system monitoring
+* `exa` for better file system viewing
+* `fzf` for fuzzy search
+* `rg` for recursive search in files
--- source/vim/install.sh
+++ source/vim/install.sh
@@ -17,13 +17,14 @@ vim -E -u NONE -c "helptags $VIM_PLUGINS/commentary/doc" -c q
 git clone -q https://github.com/jpalardy/vim-slime.git $VIM_PLUGINS/slime
 vim -E -u NONE -c "helptags $VIM_PLUGINS/slime/doc" -c q
 
+git clone -q -b 0.42.0 https://github.com/junegunn/fzf.git $VIM_PLUGINS/fzf
+vim -E -u NONE -c "helptags $VIM_PLUGINS/fzf/doc" -c q
+
 git clone -q -b v0.11 https://github.com/vim-airline/vim-airline.git $VIM_PLUGINS/vim-airline
 git clone -q https://github.com/vim-airline/vim-airline-themes.git $VIM_PLUGINS/vim-airline-themes
 vim -E -u NONE -c "helptags $VIM_PLUGINS/vim-airline/doc" -c q
 
 git clone -q https://github.com/elzr/vim-json.git $script_dir/bundle/vim-json
-
-#git clone -q -b v0.4 https://github.com/hanschen/vim-ipython-cell.git $script_dir/bundle/vim-ipython-cell
 
 git clone -q -b v0.10.2 https://github.com/VundleVim/Vundle.vim.git $script_dir/bundle/Vundle.vim
 
