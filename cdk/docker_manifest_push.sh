66ce607e950085880ce4c330af23d062d8c27c87
Danylo Vashchilenko <dan.vashchilenko@gmail.com>
2024-01-21 15:38:51 -0800

    fix manifest push

--- cdk/docker_manifest_push.sh
+++ cdk/docker_manifest_push.sh
@@ -7,7 +7,7 @@ aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --
 docker pull "${ECR_REPO}:core-amd64"
 docker pull "${ECR_REPO}:core-arm64"
 
-docker manifest rm "${ECR_REPO}:latest"
+docker manifest rm "${ECR_REPO}:latest" || true
 docker manifest create "${ECR_REPO}:latest" "${ECR_REPO}:core-amd64" "${ECR_REPO}:core-arm64"
 docker manifest annotate --arch "amd64" "${ECR_REPO}:latest" "${ECR_REPO}:core-amd64"
 docker manifest annotate --arch "arm64" "${ECR_REPO}:latest" "${ECR_REPO}:core-arm64"
@@ -16,7 +16,7 @@ docker manifest push "${ECR_REPO}:latest"
 docker pull "${ECR_REPO}:user-amd64"
 docker pull "${ECR_REPO}:user-arm64"
 
-docker manifest rm "${ECR_REPO}:user-latest"
+docker manifest rm "${ECR_REPO}:user-latest" || true
 docker manifest create "${ECR_REPO}:user-latest" "${ECR_REPO}:user-amd64" "${ECR_REPO}:user-arm64"
 docker manifest annotate --arch "amd64" "${ECR_REPO}:user-latest" "${ECR_REPO}:user-amd64"
 docker manifest annotate --arch "arm64" "${ECR_REPO}:user-latest" "${ECR_REPO}:user-arm64"
