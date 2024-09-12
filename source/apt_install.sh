153fe78475041afba0580e5455b5ad459e8bddf8
Danylo Vashchilenko <dan.vashchilenko@gmail.com>
2024-09-12 19:54:31 -0400

    add fish and use separate script

---
+++ source/apt_install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get -qy update >/dev/null \
+    &&  \
+        apt-get -qy install \
+        apt-file \
+        btop \
+        build-essential \
+        clang \
+        cmake \
+        ctop \
+        curl \
+        dnsutils \
+        exa \
+        fd-find \
+        fish \
+        fzf \
+        gettext \
+        git \
+        gnupg \
+        htop \
+        locales \
+        man \
+        ninja-build \
+        ripgrep \
+        software-properties-common \
+        sudo \
+        tmux \
+        tree \
+        tzdata \
+        wget \
+        zip \
+        zsh \
+        2>&1 >/dev/null
+
